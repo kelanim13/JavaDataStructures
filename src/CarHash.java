2a8daf5c64935cc67664dd5994ab22442397e50f
kelanim13 <lkmuelle@gmail.com>
2021-03-09 17:33:04 -0600

    modified :)

--- src/CarHash.java
+++ src/CarHash.java
@@ -1,30 +1,62 @@
-import java.util.*; 
+import java.util.*;
 
 public class CarHash {
-	
+
 	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in); 
-		
-		HashMap<String, String> car = new HashMap<String, String>(); 
-		
-		car.put("Camry", "Toyota"); 
-		car.put("Rogue", "Nissan"); 
-		car.put("Santa Fe", "Hyundai"); 
-		car.put("Accord", "Honda"); 
-		car.put("Wrangler", "Jeep"); 
-		
-		String model = ""; 
-	
+
+		Scanner input = new Scanner(System.in);
+
+		HashMap<String, String> car = new HashMap<String, String>();
+
+		car.put("Camry", "Toyota");
+		car.put("Rogue", "Nissan");
+		car.put("Santa Fe", "Hyundai");
+		car.put("Accord", "Honda");
+		car.put("Wrangler", "Jeep");
+
+		String model = "";
+
 		System.out.println("What car model are you looking for?");
-		model = input.nextLine(); 
-		
-		if(car.containsKey(model)) {
-			System.out.println("Oh, you're looking for a " + car.get(model) + " " + model + "?" + "\nOur " + car.get(model) + " selection is right over there :)"); 
+		model = input.nextLine();
+
+		if (car.containsKey(model)) {
+
+			System.out.println("Oh, you're looking for a " + car.get(model) + " " + model + "?" + "\nOur "
+					+ car.get(model) + " selection is right over there :)");
+		} else {
+			System.out.println("Sorry, but We do not have a " + model);
 		}
-		else 
-		{
-			System.out.println("Sorry, but We do not have a " + model); 
+
+		// different approach
+
+		System.out.println("What car make are you looking for?");
+		String carMake = input.nextLine();
+
+		switch (carMake) {
+
+		case "Toyota":
+			System.out.println("Our Toyota camry selection is right over there :)");
+			break;
+
+		case "Nissan":
+			System.out.println("Our Nissan Rogue selection is right over there :)");
+			break;
+
+		case "Hyundai":
+			System.out.println("Our Hyundai Santa Fe selection is right over there :)");
+			break;
+
+		case "Honda":
+			System.out.println("Our Honda Accord selection is right over there :)");
+			break;
+
+		case "Jeep":
+			System.out.println("Our Jeep Wrangler selection is right over there :)");
+			break;
+
+		default:
+			System.out.println("Sorry we do not have that make in store :(");
+			break;
 		}
 	}
 }
