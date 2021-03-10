b05a95d4fea8d49b625c2fd21e4f517070ff3694
kelanim13 <lkmuelle@gmail.com>
2021-03-10 08:08:19 -0600

    a few changes :)

--- src/LoopNums.java
+++ src/LoopNums.java
@@ -4,70 +4,59 @@ import java.util.Scanner;
 
 public class LoopNums {
 
-	public static void main(String[] args) {	
-		
+	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
-		
+
 		ArrayList<Integer> list = new ArrayList<>();
 
-		 for (int i = 0; i < 5; i++) 
-		 
-        {
+		for (int i = 0; i < 5; i++)
+
+		{
 			System.out.print("Enter a number: ");
-			Integer value = Integer.parseInt(input.nextLine()); 
-			list.add(value); 	
-        }
+			Integer value = Integer.parseInt(input.nextLine());
+			list.add(value);
+		}
 		System.out.print(list);
-		
-		System.out.println("\nSum of list: " + sum(list)); 
-		
-		System.out.println("\npoduct of list: " + product(list));
-		
-		//System.out.println("smallest number is: " + Collections.min(list));   *shortcut 
-		
-		//System.out.println("largest number is: " + Collections.max(list)); 	*shortcut
-		
-		int min = list.get(0); 
-		int max = list.get(0); 
-		
-		for (int i = 0; i < list.size(); i++)
-		{
-			if(list.get(i) >= max) {
-				max = list.get(i); 
+
+		int sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+
+		System.out.println("\nSum of list: " + sum);
+
+		int product = 1;
+
+		for (int i = 0; i < list.size(); i++) {
+			product *= list.get(i);
+		}
+
+		System.out.println("\npoduct of list: " + product);
+
+		// System.out.println("smallest number is: " + Collections.min(list)); *shortcut
+
+		// System.out.println("largest number is: " + Collections.max(list)); *shortcut
+
+		int min = list.get(0);
+		int max = list.get(0);
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) <= min) {
+				min = list.get(i);
 			}
 		}
-		
-		for (int i = 0; i < list.size(); i++)
-		{
-			if(list.get(i) <= min) {
-				min = list.get(i); 
+
+		System.out.println("the smallest number is " + min);
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) >= max) {
+				max = list.get(i);
 			}
 		}
-		
-		System.out.println("the largest number is " + max); 
-		System.out.println("the smallest number is " + min); 
-		
+
+		System.out.println("the largest number is " + max);
+
 	}
-		
-	
-	public static int sum(ArrayList<Integer>list) {
-		int sum = 0; 
-		for (int i = 0; i < list.size(); i++)
-		{
-			sum += list.get(i); 
-		}
-		return sum; 
-	}
-	
-	public static int product(ArrayList<Integer>list) {
-		int product = 1; 
-		
-		for (int i = 0; i < list.size(); i++)
-		{
-			product *= list.get(i); 
-		}			
-		return product;
-	}
-	
-	
-}	
+
+}
